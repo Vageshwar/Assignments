8a3ed68b9cabcdf3a59ad5b8b21932632d1d3398
Vageshwar Yadav <55757589+Vageshwar@users.noreply.github.com>
2021-10-20 17:12:27 +0530

    Update code2.js

--- code2.js
+++ code2.js
@@ -1,2 +1,3 @@
+// localeCompare : tells if string comes before, after or in same order in given locale.
 const sortAlphaNum = (a, b) => a.localeCompare(b, 'en', { numeric: true })
 console.log(['A1', 'A10', 'A11', 'A12', 'A2', 'A3', 'A4', 'B10', 'B2', 'F1', 'F12', 'F3'].sort(sortAlphaNum))
